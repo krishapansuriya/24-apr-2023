4fe80efdc5373a7aa62b68e877f1b0e391b96262
krishapansuriya <131764738+krishapansuriya@users.noreply.github.com>
2023-06-07 11:23:24 +0530

    Add files via upload

---
+++ variable1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c;
+	a=30;
+	b=10;
+	c=a;
+	a=b;
+	b=c;
+	printf("Value of A=%d",a);
+	printf("\nValue of B=%d",b);
+}
---
+++ variable2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int a,b;
+	a=10;
+	b=30;
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	printf("Value of A=%d",a);
+	printf("\nValue of B=%d",b);
+}
